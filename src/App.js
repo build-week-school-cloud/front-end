114063580ea94a510acb3632e4fb7b6623cef9f3
Nikki Lin <nikkiylin@gmail.com>
2020-03-02 11:39:20 -0800

    inital

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navigation from './Navigation';
-import { Route } from 'react-router-dom';
-import Login from './Login';
-import SignUp from './Sign';
 
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Route path='/'>
-        <Login/>
-      </Route>
-      <Route path='/sign-up'>
-        <SignUp/>
-      </Route>
-      {/* <Route path='/adminstration'>
-        <Administration/>
-      </Route>
-      <Route path='/'>
-        <SignUp/>
-      </Route>
-      <Route path='/sign-up'>
-        <SignUp/>
-      </Route> */}
+    Hello 
     </div>
   );
 }
