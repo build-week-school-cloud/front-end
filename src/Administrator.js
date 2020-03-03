261b8fe505d34a4972891d03bd8e621c91112177
Nikki Lin <nikkiylin@gmail.com>
2020-03-03 08:27:51 -0800

    added administrator and volunteer files

--- src/Administrator.js
+++ src/Administrator.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Administrator = () => {
-return (
-    <div className="administrator">
-        <h1>Administrator</h1>
-        
-    </div>
-)
+    const toDoList = [
+        "Prepare notes",
+        "Check attence",
+        "Upload class materials",
+        "Help student to do homework",
+        "Make sure student understand pronblems"
+    ];
+    const [list, setList] = useState(toDoList);
+    const [newItem, setNewItem] = useState("");
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const newList = [...list, newItem];
+        setList(newList);
+    };
+
+    const onChangeHandler = (event) => {
+        setNewItem(event.target.value);
+    };
+    
+
+
+    return (
+        <div className="administrator">
+            <h1>Administrator</h1>
+            <h3>Todo List</h3>
+            {list.map( item => <div className="item">{item} </div> )  }
+            <form onSubmit={submitHandler}>
+                <input onChange={onChangeHandler} id="add" type="text" />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
 
 }
 
--- src/Volunteer.js
+++ src/Volunteer.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 const Volunteer = () => {
+    const toDoList = [
+        "Prepare notes",
+        "Check attence",
+        "Upload class materials",
+        "Help student to do homework",
+        "Make sure student understand pronblems"
+    ];
+
+    
     return (
         <div className="volunteer">
             <h1>Volunteer</h1>
+            <h3>To Do List</h3>
+            {toDoList.map( item => <div className="item"><input type="checkbox" />{item}</div>)}
         </div>
     )
 }
 
 export default Volunteer;
+
+           
