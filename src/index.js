87476e88ba5a3990afe20257b89735c1a5378bfb
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-03 15:14:18 -0500

    Re-added provider, store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-<Router><App /></Router>, document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
