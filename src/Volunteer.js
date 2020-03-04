f0ba25d1b7ef44da59fbd0aa45b0cbcc71b5993e
Nikki Lin <nikkiylin@gmail.com>
2020-03-04 12:27:13 -0800

    added complete button on Volunteer.js file

--- src/Volunteer.js
+++ src/Volunteer.js
@@ -9,13 +9,22 @@ const Volunteer = () => {
         "Make sure student understand pronblems"
     ];
 
+    const handleSubmit = (form) => {
+        form.reset();
+    }
     
     return (
+        <form onSubmit={handleSubmit}>
         <div className="volunteer">
             <h1>Volunteer</h1>
             <h3>To Do List</h3>
             {toDoList.map( item => <div className="item"><input type="checkbox" />{item}</div>)}
+            <div className="complete">
+                <labe htmlFor="complete"></labe>
+            <input id="complete" type="submit" value="Complete" />
+            </div>
         </div>
+        </form>
     )
 }
 
