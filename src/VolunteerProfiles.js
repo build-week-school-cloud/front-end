165beab6f14f8ebe0ea31d0ac1c9d1d5a80f3204
Nikki Lin <nikkiylin@gmail.com>
2020-03-04 13:24:31 -0800

    added axios request with different api to test if it works. works!

--- src/VolunteerProfiles.js
+++ src/VolunteerProfiles.js
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import {dummyData} from "./dummy-data";
 import VolunteerProfile from "./VolunteerProfile";
 import SearchForm from "./SearchForm";
 
 export default function VolunteerProfiles(props) {
   // TODO: Add useState to track data from useEffect
-  const [profiles, setProfiles] = useState(dummyData);
-  const [originalProfiles, setOriginalProfiles] = useState(dummyData);
-  const [searchText, setSearchText] = useState("");
+  // const [profiles, setProfiles] = useState(dummyData);
+  // const [originalProfiles, setOriginalProfiles] = useState(dummyData);
+  // const [searchText, setSearchText] = useState("");
 
+  const [profiles, setProfiles] = useState([]);
+  const [originalProfiles, setOriginalProfiles] = useState([]);
+  const [searchText, setSearchText] = useState("");
   
-//   useEffect(() => {
-//     // TODO: Add API Request here - must run in `useEffect`
-//     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  useEffect(() => {
+    // TODO: Add API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     
-//     const getList = () => {
-//       axios
-//       .get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
-//       .then(response => {
-//         console.log(response.data);
-//         setList(response.data.results);
-//         setOriginalList(response.data.results);
-//       })
-//       .catch(error => {
-//         console.log("Server Error", error);
-//       })
-//     }
-//     getList();
-//   }, []);
+    const getList = () => {
+      axios
+      .get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response.data);
+        setProfiles(response.data.results);
+        setOriginalProfiles(response.data.results);
+      })
+      .catch(error => {
+        console.log("Server Error", error);
+      })
+    }
+    getList();
+  }, []);
 
   useEffect(() => {
     const results = originalProfiles.filter(character =>
